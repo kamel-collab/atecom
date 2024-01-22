52736148a849e43428c01fb3171760f9730e61d4
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2024-01-22 14:33:55 +0100

    8

--- lib/widgets/products.dart
+++ lib/widgets/products.dart
@@ -45,8 +45,35 @@ class _ProductsState extends State<Products> {
       ),
       child: Column(
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF4C53A5),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text(
+                  "-50 %",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Icon(
+                Icons.favorite_border,
+                color: Colors.red,
+              )
+            ],
+          ),
           InkWell(
-            onTap: () {},
+            onTap: () {
+              Navigator.pushNamed(context, 'itemPage',
+                  arguments: widget.products[index]);
+            },
             child: Container(
               margin: const EdgeInsets.all(10),
               child: Image.asset(widget.products[index]["image"]),
@@ -75,6 +102,26 @@ class _ProductsState extends State<Products> {
                 fontSize: 15,
                 color: Color(0xFF4C53A5),
               ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.products[index]["prix"] + " DA",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color(0xFF4C53A5),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(
+                  Icons.shopping_cart_checkout,
+                  color: Color(0xFF4C53A5),
+                )
+              ],
             ),
           ),
         ],
