1760033cdfdbeff286d8ce6b5a52b78967ec3a4d
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2024-01-22 10:27:32 +0100

    6

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,3 +1,4 @@
+import 'package:atc/widgets/categories.dart';
 import 'package:atc/widgets/home_app_bar.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
@@ -15,15 +16,70 @@ class Home extends StatelessWidget {
           Expanded(
             child: ListView(
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFFEDECF2),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(35),
-                      topRight: Radius.circular(35),
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFEDECF2),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(35),
+                          topRight: Radius.circular(35),
+                        ),
+                      ),
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 15),
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 5),
+                              height: 50,
+                              width: 300,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Search ..."),
+                              ),
+                            ),
+                            const Spacer(),
+                            const Icon(
+                              Icons.search,
+                              size: 27,
+                              color: Color(0xFF4C53A5),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  child: Text('ok'),
+                    Container(
+                      color: Color(0xFFEDECF2),
+                      child: Column(
+                        children: [
+                          Categories(),
+                          //items
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 10),
+                            child: const Text(
+                              'Best Selling',
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF4C53A5),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
