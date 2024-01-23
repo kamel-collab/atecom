a66afaa235832f5dd5ac4e4ba1d0ca6cd5c4ebfe
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2024-01-23 10:02:56 +0100

    11

--- lib/bloc/products/data/productsProvider.dart
+++ lib/bloc/products/data/productsProvider.dart
@@ -2,7 +2,8 @@ class ProductsProvider {
 // creer ne function getProducts   doit etre une promesse
 //elle retourne une list de Product
 // depuis un api en utilsant http
-  const String token =
+//"bahmedkamel.com", 'wp-json/wc/v3/products'
+  static const String token =
       "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDYwMDAzMzIsImV4cCI6MTcwNjAwMzkzMiwiZW1haWwiOiJhZG1pbkBiYWhtZWRrYW1lbC5jb20iLCJpZCI6IjEiLCJzaXRlIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20iLCJ1c2VybmFtZSI6ImFkbWluIiwiaXNzIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20ifQ.9wPaG9FSwxiajxdk-PzWygqhF3Jc4YnBIUjTgG2le1g";
 }
 
