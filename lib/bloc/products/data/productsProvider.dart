180f9dd607caa4b372239ab5072358e973889845
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2024-01-23 10:57:45 +0100

    mm

--- lib/bloc/products/data/productsProvider.dart
+++ lib/bloc/products/data/productsProvider.dart
@@ -4,7 +4,7 @@ class ProductsProvider {
 // depuis un api en utilsant http
 //"bahmedkamel.com", 'wp-json/wc/v3/products'
   static const String token =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDYwMDAzMzIsImV4cCI6MTcwNjAwMzkzMiwiZW1haWwiOiJhZG1pbkBiYWhtZWRrYW1lbC5jb20iLCJpZCI6IjEiLCJzaXRlIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20iLCJ1c2VybmFtZSI6ImFkbWluIiwiaXNzIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20ifQ.9wPaG9FSwxiajxdk-PzWygqhF3Jc4YnBIUjTgG2le1g";
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDYwMDM4MjMsImV4cCI6MTcwNjAwNzQyMywiZW1haWwiOiJhZG1pbkBiYWhtZWRrYW1lbC5jb20iLCJpZCI6IjEiLCJzaXRlIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20iLCJ1c2VybmFtZSI6ImFkbWluIiwiaXNzIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20ifQ.mv-i8cvV11D4wpPWmZAn3xG6YWqryxOHkAUdhSMUU1A";
 }
 
 class Product {
